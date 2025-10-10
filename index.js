97be4dfda80907e1e7f9753608dd8eabbd5737ca
buildwithsuraj-lang <buildwithsuraj@gmail.com>
2025-10-10 06:41:38 +0530

    add console

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express=require('express')
-const app= express();
-const route=require('../Router/route')
-const data=require('../models/userdata');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const route = require("../Router/route");
+const data = require("../models/userdata");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/',route);
-   
-app.listen(process.env.Port,()=>{
-    console.log('server is running')
-})
+app.use("/", route);
+console.log("hello");
+app.listen(process.env.Port, () => {
+  console.log("server is running");
+});
