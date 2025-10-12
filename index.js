411ab8a035dd30603a8a99727de39846294c866f
buildwithsuraj-lang <buildwithsuraj@gmail.com>
2025-10-12 19:47:44 +0530

    enable cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
